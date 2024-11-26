37ca44ffbda436ea9844bdd16b35dcd58355cbf7
polina efimova <ffforward@bk.ru>
2024-11-26 13:54:54 +0300

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Полина Ефимова on 26.11.24.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
